40965b32009490ed66c15e0da60d2d194d23eaf3
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-18 16:22:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,8 @@
 katz_deli = []
 
 def line(katz_deli)
-  katz_deli.map.with_index { |name, index| puts "The line is currently: #{index}. #{name}" }
+  katz_deli.map.with_index { |name, index| [name, index] }
+  puts "The line is currently: " + "#{katz_deli.map.with_index}""
   if katz_deli.length == 0
     puts "The line is currently empty."
   end
