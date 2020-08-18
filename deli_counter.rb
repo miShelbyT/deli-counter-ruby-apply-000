51f2d39ba8dc1ade5836433f592b74ddb439a72d
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-18 16:11:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(katz_deli, end_of_line_name)
   katz_deli.push(end_of_line_name)
-  katz_deli.with_index |end_of_line_name| |index|
+  katz_deli.with_index |end_of_line_name, index|
   puts "Welcome, #{end_of_line_name}. Your position in line is #{index}."
 end
 
